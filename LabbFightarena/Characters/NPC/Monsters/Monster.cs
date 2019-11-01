8b213f02a2656a867cb661ececf8e946d83bb464
PaffaLon <tobiasbjornolsson@gmail.com>
2019-11-01 15:41:33 +0100

    Added logi for monster

---
+++ LabbFightarena/Characters/NPC/Monsters/Monster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabbFightarena.Characters.NPC.Monsters
+{
+    class Monster
+    {
+        public string Name { get; set; }
+        public int MaxHealth { get; set; }
+        public int Health { get; set; }
+        public int Strength { get; set; }
+    }
+}
