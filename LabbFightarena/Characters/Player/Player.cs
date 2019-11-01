eb6fcfcc78e7cd3de0eeef43d580664a64ec2e1d
PaffaLon <tobiasbjornolsson@gmail.com>
2019-11-01 15:40:28 +0100

    base logi for player

---
+++ LabbFightarena/Characters/Player/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabbFightarena.Characters.Player
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int MaxHealth { get; set; }
+        public int Health { get; set; }
+        public int Strength { get; set; }
+
+
+    }
+}
