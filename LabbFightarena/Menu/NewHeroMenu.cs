17252162bfcb6f5321b1a0ec195eac3491d7bb75
Tobias Olsson <Olsson>
2019-11-04 16:00:01 +0100

    Started working with the menu for creating a new hero.

---
+++ LabbFightarena/Menu/NewHeroMenu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabbFightarena.Menu
+{
+    public class NewHeroMenu
+    {
+        public void Content()
+        {
+            string[] unselcetdMenuItems = new string[0];
+            string[] selectedMenuItems = new string[0];
+
+            selectedMenuItems[0] = ("Name :");
+        }
+    }
+}
