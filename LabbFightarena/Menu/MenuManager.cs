8da8576193c2149af62a170d7889ff8fd31aadb1
Tobias Olsson <Olsson>
2019-11-11 10:26:18 +0100

    Cleaned up code. Added a method.

--- LabbFightarena/Menu/MenuManager.cs
+++ LabbFightarena/Menu/MenuManager.cs
@@ -29,11 +29,7 @@ namespace LabbFightarena.Menu
         //Loads the active menu for the user.
         public void LoadMenu()
         {
-            if (Console.CursorVisible == true)
-                Console.CursorVisible = false;
-            
-            if(mainMenu.Elements != null)
-                mainMenu.Elements.Clear();
+            ClearMenuElements();
 
             if (ActiveApplicationMenu == (int)ApplicationMenus.MainMenu)
             {
@@ -47,7 +43,6 @@ namespace LabbFightarena.Menu
                     heroMenu.Content(heroMenu.Button);
                     foreach (var item in heroMenu.getMenuItems)
                         heroMenu.Elements.Add(item);
-
             }
             else if (ActiveApplicationMenu == (int)ApplicationMenus.CombatLog)
             {
@@ -84,80 +79,81 @@ namespace LabbFightarena.Menu
             ConsoleKeyInfo cki;
             cki = Console.ReadKey();
 
-            switch (ActiveApplicationMenu)
+            if(ActiveApplicationMenu == (int)ApplicationMenus.MainMenu)
             {
-                case (int)ApplicationMenus.CombatLog:
-                    break;
+                if (cki.Key.GetHashCode() == 38)
+                {
+                    mainMenu.Button -= 1;
+                    if (mainMenu.Button < 0)
+                        mainMenu.Button = 0;
+                }
+                else if (cki.Key.GetHashCode() == 40)
+                {
+                    mainMenu.Button += 1;
+                    if (mainMenu.Button > 3)
+                        mainMenu.Button = 3;
+                }
+                else if (cki.Key.GetHashCode() == 13 && mainMenu.Button == (int)MainMenu.Buttons.Play)
+                {
+                    ActiveApplicationMenu = (int)ApplicationMenus.HeroMenu;
+                    mainMenu.Button = (int)HeroMenu.Buttons.NewHero;
+                    LoadMenu();
 
-                case (int)ApplicationMenus.HeroMenu:
-                    if (cki.Key.GetHashCode() == 38)
-                    {
-                        heroMenu.Button -= 1;
-                        if (heroMenu.Button < 0)
-                            heroMenu.Button = 0;
-                    }
-                    else if (cki.Key.GetHashCode() == 40)
-                    {
-                        heroMenu.Button += 1;
-                        if (heroMenu.Button > 2)
-                            heroMenu.Button = 2;
-                    }
-                    else if (cki.Key.GetHashCode() == 13 && heroMenu.Button == (int)HeroMenu.Buttons.NewHero)
-                    {
-                        Console.Clear();
-                        Console.WriteLine("You can soon generate a new hero.");
-                        Thread.Sleep(1000);
-                    }
-                    else if (cki.Key.GetHashCode() == 13 && heroMenu.Button == (int)HeroMenu.Buttons.LoadHero)
-                    {
-                        Console.Clear();
-                        Console.WriteLine("The feature: Load hero, has not been implemented.");
-                        Thread.Sleep(2500);
-                    }
-                    else if (cki.Key.GetHashCode() == 13 && heroMenu.Button == (int)HeroMenu.Buttons.Exit)
-                    {
-                        ActiveApplicationMenu = (int)ApplicationMenus.MainMenu;
-                        mainMenu.Button = (int)MainMenu.Buttons.Play;
+                }
+                else if (cki.Key.GetHashCode() == 13 && mainMenu.Button == (int)MainMenu.Buttons.ScoreBoard)
+                {
+                    Console.Clear();
+                    Console.WriteLine("The Scoreboard has not jet been implemented.");
+                    Thread.Sleep(1000);
+                }
+                else if (cki.Key.GetHashCode() == 13 && mainMenu.Button == (int)MainMenu.Buttons.CombatLog)
+                {
+                    Console.Clear();
+                    Console.WriteLine("The View last combat log has not jet been implemnted.");
+                    Thread.Sleep(1000);
+                }
+                else if (cki.Key.GetHashCode() == 13 && mainMenu.Button == (int)MainMenu.Buttons.Exit)
+                {
+                    Environment.Exit(0);
+                }
+            }
+            if (ActiveApplicationMenu == (int)ApplicationMenus.HeroMenu)
+            {
+                if (cki.Key.GetHashCode() == 38)
+                {
+                    heroMenu.Button -= 1;
+                    if (heroMenu.Button < 0)
+                        heroMenu.Button = 0;
+                }
+                else if (cki.Key.GetHashCode() == 40)
+                {
+                    heroMenu.Button += 1;
+                    if (heroMenu.Button > 2)
+                        heroMenu.Button = 2;
+                }
+                else if (cki.Key.GetHashCode() == 13 && heroMenu.Button == (int)HeroMenu.Buttons.NewHero)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You can soon generate a new hero.");
+                    Thread.Sleep(1000);
+                }
+                else if (cki.Key.GetHashCode() == 13 && heroMenu.Button == (int)HeroMenu.Buttons.LoadHero)
+                {
+                    Console.Clear();
+                    Console.WriteLine("The feature: Load hero, has not been implemented.");
+                    Thread.Sleep(2500);
+                }
+                else if (cki.Key.GetHashCode() == 13 && heroMenu.Button == (int)HeroMenu.Buttons.Exit)
+                {
+                    ActiveApplicationMenu = (int)ApplicationMenus.MainMenu;
+                    mainMenu.Button = (int)MainMenu.Buttons.Play;
 
-                    }break;
-                default:
-                    if (cki.Key.GetHashCode() == 38)
-                    {
-                        mainMenu.Button -= 1;
-                            if(mainMenu.Button < 0)
-                                mainMenu.Button = 0;
-                    }
-                    else if (cki.Key.GetHashCode() == 40)
-                    {
-                        mainMenu.Button += 1;
-                        if (mainMenu.Button > 3)
-                            mainMenu.Button = 3;
-                    }
-                    else if (cki.Key.GetHashCode() == 13 && mainMenu.Button == (int)MainMenu.Buttons.Play)
-                    {
-                        ActiveApplicationMenu = (int)ApplicationMenus.HeroMenu;
-                        mainMenu.Button = (int)HeroMenu.Buttons.NewHero;
-                        LoadMenu();
-                        
-                    }
-                    else if (cki.Key.GetHashCode() == 13 && mainMenu.Button == (int)MainMenu.Buttons.ScoreBoard)
-                    {
-                        Console.Clear();
-                        Console.WriteLine("The Scoreboard has not jet been implemented.");
-                        Thread.Sleep(1000);
-                    }
-                    else if (cki.Key.GetHashCode() == 13 && mainMenu.Button == (int)MainMenu.Buttons.CombatLog)
-                    {
-                        Console.Clear();
-                        Console.WriteLine("The View last combat log has not jet been implemnted.");
-                        Thread.Sleep(1000);
-                    }
-                    else if (cki.Key.GetHashCode() == 13 && mainMenu.Button == (int)MainMenu.Buttons.Exit)
-                    {
-                        Environment.Exit(0);
-                    }
-                    break;
-            }        
+                }
+            }
+            if (ActiveApplicationMenu == (int)ApplicationMenus.CombatLog)
+            {
+
+            }
             LoadMenu();
         }
 
@@ -167,12 +163,12 @@ namespace LabbFightarena.Menu
             switch (ActiveApplicationMenu)
             {
                 case (int)ApplicationMenus.HeroMenu:
-                    break;
                     foreach (var item in heroMenu.Elements)
                     {
                         Console.SetCursorPosition((Console.WindowWidth - item.Length) / 2, Console.CursorTop);
                         Console.WriteLine(item + Environment.NewLine);
                     }
+                    break;
                 default:
                     foreach (var item in mainMenu.Elements)
                     {
@@ -181,6 +177,19 @@ namespace LabbFightarena.Menu
                     }
                     break;
             }
+        }
+    
+        //Empties the list that contains the order the menu items has to be printed.
+        private void ClearMenuElements()
+        {
+            if (Console.CursorVisible == true)
+                Console.CursorVisible = false;
+
+            if (mainMenu.Elements != null)
+                mainMenu.Elements.Clear();
+
+            if (heroMenu.Elements != null)
+                heroMenu.Elements.Clear();
         }
     }
 }
