8f7ca618691f40c4aef74c9a21465f47b2dd67de
Tobias Olsson <tobiasbjornolsson@gmail.com>
2019-11-26 19:25:24 +0100

    Added methods for maning the two characters.

--- LabbFightarena/Characters/CharacterManager.cs
+++ LabbFightarena/Characters/CharacterManager.cs
@@ -1,22 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using LabbFightarena.Characters.Players;
+using LabbFightarena.Characters.Monsters;
 
 namespace LabbFightarena.Characters
 {
     //Managers the creation & usage of characters.
     public class CharacterManager
     {
-        
-        
-        private void CreateNewCharacter()
+        static public List<Player> Players { get; set; }
+        static public List<Monster> Monsters { get; set; } 
+
+        public CharacterManager()
         {
 
         }
 
-        private void GetCharacter()
+#region Player
+
+        public void AddPlayer(Player player)
+        {
+            Players.Add(player);
+        }
+
+        public void GetPlayer()
+        {
+            
+        }
+
+        public void DeletePlayer()
         {
 
         }
+
+        #endregion Player
+#region Monster
+        public void AddMonster(Monster monster)
+        {
+            Monsters.Add(monster);
+        }
+
+        public void GetMonster()
+        {
+
+        }
+
+        public void DeleteMonster()
+        {
+
+        }
+
+#endregion Monster
     }
 }
